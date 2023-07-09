4dd763021a4a9692a1371b63ea90ef2e48423ed7
Cemal Gunes <cemal.gunes@logo.com.tr>
2023-07-09 16:20:57 +0300

    FrogRiverOne

--- CodilityLessons/CodilityLessons.Test/LessonsTests.cs
+++ CodilityLessons/CodilityLessons.Test/LessonsTests.cs
@@ -3,39 +3,39 @@ namespace CodilityLessons.Test
     public class LessonsTests
     {
         [SetUp]
-        public void Setup() 
+        public void Setup()
         {
         }
 
         [Test]
         [TestCase(1041, ExpectedResult = 5)]
         [TestCase(32, ExpectedResult = 0)]
-        public int BinaryGapTest(int N)  
+        public int BinaryGapTest(int N)
         {
-            return new BinaryGap().Solution(N); 
+            return new BinaryGap().Solution(N);
         }
 
         [Test]
-        [TestCase(new int[] { 3,8,9,7,6 } , 3, ExpectedResult =  new int[] { 9, 7, 6, 3, 8 })]
+        [TestCase(new int[] { 3, 8, 9, 7, 6 }, 3, ExpectedResult = new int[] { 9, 7, 6, 3, 8 })]
         [TestCase(new int[] { 0, 0, 0 }, 1, ExpectedResult = new int[] { 0, 0, 0 })]
-        [TestCase(new int[] { 1, 2, 3, 4 }, 4, ExpectedResult = new int[] { 1, 2, 3, 4 })] 
-        public int[] CyclicRotationTest(int[] A, int K)  
+        [TestCase(new int[] { 1, 2, 3, 4 }, 4, ExpectedResult = new int[] { 1, 2, 3, 4 })]
+        public int[] CyclicRotationTest(int[] A, int K)
         {
             return new CyclicRotation().Solution(A, K);
         }
 
         [Test]
-        [TestCase(new int[] { 9,3,9,3,9,7,9 }, ExpectedResult = 7)] 
+        [TestCase(new int[] { 9, 3, 9, 3, 9, 7, 9 }, ExpectedResult = 7)]
         public int OddOccurrencesInArrayTest(int[] A)
         {
             return new OddOccurrencesInArray().Solution(A);
         }
 
         [Test]
-        [TestCase(10,85,30, ExpectedResult = 3)]
-        public int FrogJmpTest(int X, int Y, int D) 
+        [TestCase(10, 85, 30, ExpectedResult = 3)]
+        public int FrogJmpTest(int X, int Y, int D)
         {
-            return new FrogJmp().Solution(X, Y, D); 
+            return new FrogJmp().Solution(X, Y, D);
         }
 
         [Test]
@@ -50,6 +50,13 @@ namespace CodilityLessons.Test
         public int TapeEquilibriumTest(int[] A)
         {
             return new TapeEquilibrium().Solution(A);
+        }
+
+        [Test]
+        [TestCase(5, new int[] { 1, 3, 1, 4, 2, 3, 5, 4 }, ExpectedResult = 6)]
+        public int FrogRiverOneTest(int X, int[] A)
+        {
+            return new FrogRiverOne().Solution(X, A);
         }
     }
 }
