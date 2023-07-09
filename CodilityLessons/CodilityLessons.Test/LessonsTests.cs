948c59fc7262080b3f494d5263f5e3190ba312d4
Cemal Gunes <cemal.gunes@logo.com.tr>
2023-07-09 16:07:11 +0300

    added unit tests

--- CodilityLessons/CodilityLessons.Test/LessonsTests.cs
+++ CodilityLessons/CodilityLessons.Test/LessonsTests.cs
@@ -8,6 +8,44 @@ namespace CodilityLessons.Test
         }
 
         [Test]
+        [TestCase(1041, ExpectedResult = 5)]
+        [TestCase(32, ExpectedResult = 0)]
+        public int BinaryGapTest(int N)  
+        {
+            return new BinaryGap().Solution(N); 
+        }
+
+        [Test]
+        [TestCase(new int[] { 3,8,9,7,6 } , 3, ExpectedResult =  new int[] { 9, 7, 6, 3, 8 })]
+        [TestCase(new int[] { 0, 0, 0 }, 1, ExpectedResult = new int[] { 0, 0, 0 })]
+        [TestCase(new int[] { 1, 2, 3, 4 }, 4, ExpectedResult = new int[] { 1, 2, 3, 4 })] 
+        public int[] CyclicRotationTest(int[] A, int K)  
+        {
+            return new CyclicRotation().Solution(A, K);
+        }
+
+        [Test]
+        [TestCase(new int[] { 9,3,9,3,9,7,9 }, ExpectedResult = 7)] 
+        public int OddOccurrencesInArrayTest(int[] A)
+        {
+            return new OddOccurrencesInArray().Solution(A);
+        }
+
+        [Test]
+        [TestCase(10,85,30, ExpectedResult = 3)]
+        public int FrogJmpTest(int X, int Y, int D) 
+        {
+            return new FrogJmp().Solution(X, Y, D); 
+        }
+
+        [Test]
+        [TestCase(new int[] { 2, 3, 1, 5 }, ExpectedResult = 4)]
+        public int PermMissingElemTest(int[] A)
+        {
+            return new PermMissingElem().Solution(A);
+        }
+
+        [Test]
         [TestCase(new int[] { 3, 1, 2, 4, 3 }, ExpectedResult = 1)]
         public int TapeEquilibriumTest(int[] A)
         {
